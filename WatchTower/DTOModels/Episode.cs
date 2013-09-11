a7dd470b7e0acf27a5b33a435e6bb23bfda4124c
knowspick <knowspick@gmail.com>
2013-09-11 17:43:11 +0200

    Switch over to OrmLite

---
+++ WatchTower/DTOModels/Episode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.DataAnnotations;
+
+namespace WatchTower.DTOModels
+{
+    class Episode
+    {
+        [AutoIncrement] 
+        public Int64 Id { get; set; }
+
+        [Required]
+        public Int64 SeriesId { get; set; }
+
+        public Series Series { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int SeasonNo { get; set; }
+        [Required]
+        public int EpisodeNo { get; set; }
+        public string FileFullPath { get; set; }
+        [Required]
+        public DateTime DateAddedToCollection { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastUpdated { get; set; }
+
+        public virtual ICollection<ProfileEpisodeRel> ProfileEpisodeRel { get; set; }
+    }
+
+}
---
+++ WatchTower/DTOModels/ProfileEpisodeRel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchTower.DTOModels
+{
+    class ProfileEpisodeRel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 Id { get; set; }
+        [Required]
+        public Int64 ProfileId { get; set; }
+        [Required]
+        public Int64 EpisodeId { get; set; }
+        [Required]
+        public Boolean Played { get; set; }
+        [Required]
+        public Boolean Watched { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastUpdated { get; set; }
+
+        public virtual Episode Episode { get; set; }
+        public virtual Profile Profile { get; set; }
+    }
+}
